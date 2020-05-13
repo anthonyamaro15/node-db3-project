ec15b1a12609e9b8ca27bc66103527759d059a99
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-13 14:41:39 -0700

    ready

--- knexfile.js
+++ knexfile.js
@@ -27,6 +27,10 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
   },
+  pool: {
+    min: 2,
+    max: 10,
+  },
   migrations: {
     directory: "./data/migrations",
   },
